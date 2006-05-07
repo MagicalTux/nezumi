d872f69b85130da5708ed68e8b9c7e6a02b48f84
akrus <akrus@flygroup.st>
2006-05-07 16:36:10 +0000

    * Fixed: critical bug with Pressure & Ice Wall causing server crash [akrus]

--- src/map/skill.c
+++ src/map/skill.c
@@ -1423,16 +1423,6 @@ int skill_attack(int attack_type, struct block_list* src, struct block_list *dsr
 			clif_damage(src, bl, tick, dmg.amotion, dmg.dmotion, damage, 0, 0, 0);
 			break;
 		case PA_PRESSURE:
-			if(sc_data[SC_HIDING].timer != -1)
-			{
-				status_change_end(bl, SC_HIDING, -1);
-				return 0;
-			}
-			if(sc_data[SC_CLOAKING].timer != -1)
-			{
-				status_change_end(bl, SC_CLOAKING, -1);
-				return 0;
-			}
 			clif_skill_damage(dsrc, bl, tick, dmg.amotion, dmg.dmotion, damage, dmg.div_, skillid, (lv != 0) ? lv : skilllv, (skillid == 0) ? 5 : type);
 		case NPC_SELFDESTRUCTION:
 			break;
